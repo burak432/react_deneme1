1afa7817c664999e5f003dc93754001b82dfe0a6
Burak Atilla <burak.atilla@gmail.com>
2022-02-12 19:38:41 +0300

    1 | deneme

---
+++ src/components/deneme1.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Deneme1() {
+  return (
+    <div>
+      <h1>Deneme 123</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+        voluptatum fugiat in ab nemo asperiores ipsa error dignissimos, amet
+        praesentium.
+      </p>
+    </div>
+  );
+}
+
+export default Deneme1;
